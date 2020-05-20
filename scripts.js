2cb35a67d03faa760fd862314fcedfbf57601e72
Dane <danecodesnc@gmail.com>
2020-05-20 14:11:31 -0400

    more functions

--- scripts.js
+++ scripts.js
@@ -24,13 +24,23 @@ function updateButton() {
 
 function skip() {
     console.log(this.dataset.skip);
-    video.currentTime += (this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+   video[this.name] = (this.value);
+}
+
+function handleProgress() {
+    
+}
 //event listeners
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
